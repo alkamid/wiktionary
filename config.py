be787d42bdb7541ef2020bb2cf03660e6da0bca0
alkamid <ak763@cam.ac.uk>
2013-11-21 21:33:55 +0000

    configuration files for paths (local/sever)

---
+++ config.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+path = dict(
+    scripts = '/data/project/alkamidbot/scripts/'
+    dumps = '/public/datasets/public/plwiktionary/'
+    pagecounts = '/public/pagecounts/pagecounts-raw/'
+)
