f2a1ade7c41a4790395befd1c8246a6e48bdfa56
alkamid <ak763@cam.ac.uk>
2014-05-24 13:49:19 +0100

    little refactoring

--- frequency.py
+++ frequency.py
@@ -1,9 +1,8 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 
-# frekwencja - tworzy listę na podstawie tej z angielskiego uniwersytetu
+# missing French words, based on the frequency list from http://corpus.leeds.ac.uk/frqc/internet-fr.num
 
-import os
 import codecs
 import pywikibot
 import datetime
@@ -13,60 +12,59 @@ from klasa import *
 
 def main():
 	site = pywikibot.getSite()
-	page_list = pywikibot.Page(site, u'Portal:Francuski/potrzebne')
-	lista = []
-	inp = codecs.open('%sinne/lista_franc.txt' % config.path['scripts'], encoding='utf-8')
-	data = datetime.datetime.now().strftime("%Y-%m-%d")
+	outputPage = pywikibot.Page(site, u'Portal:Francuski/potrzebne')
+        excludedPage = pywikibot.Page(site, u'Portal:Francuski/potrzebne/wykluczone')
+	excludedList = excludedPage.get().split('\n')
+	date = datetime.datetime.now().strftime("%Y-%m-%d")
+
+        inp = codecs.open('%sinne/lista_franc.txt' % config.path['scripts'], encoding='utf-8')
+
+        wordList = [line.split()[2].split('|')[0] for line in inp]
 
 	re_stary = re.compile(u'(\*.*?\n)\[\[Kat', re.DOTALL)
 
-	for line in inp:
-		lista.append(line.split())
+        text = u''
+	counter = 0
 	
-	text = ''
-	licz = 0
-	excluded = []
-	excluded.append(u'france')
-	excluded.append(u'parce')
-	
-	for a in lista:
-		
-		if a[2] not in excluded:
+        startPoint = 310 #all the words < 300 already exist, no point checking there
+
+	for word in wordList[startPoint:]:
+
+                if word not in excludedList and len(word)>1:
 			try:
-				haslo = Haslo(a[2])
+				haslo = Haslo(word)
 			except urllib2.HTTPError:
 				pass
 			except sectionsNotFound:
 				pass
 			except WrongHeader:
-				text += '*[[' + a[2] + ']] - problem z nagłówkiem' + '\n'
+				text += u'*[[' + word + ']] - problem z nagłówkiem' + '\n'
 			else:
 				if haslo.type == 0:
-					text += '*[[' + a[2] + ']] - przekierowanie' + '\n'
+					text += u'*[[' + word + ']] - przekierowanie' + '\n'
 				elif haslo.type == 1:
-					text += '*[[' + a[2] + ']]' + '\n'
-					licz = licz + 1
+					text += u'*[[' + word + ']]' + '\n'
+					counter += 1
 				elif haslo.type == 2:
-					print 'nieznany błąd'
+					print u'*--------* nieznany błąd (haslo.type=2) *----------------*'
 				else:
-
 					found = 0
 					try: haslo.listLangs
 					except AttributeError:
 						pass
 					else:
-						for sekcja in haslo.listLangs:
-							if sekcja.lang == u'francuski':
+						for section in haslo.listLangs:
+							if section.lang == u'francuski':
 								found = 1
 						if not found:
-							text += '*[[' + a[2] + ']]' + '\n'
-							licz = licz + 1
+							text += u'*[[' + word + ']]' + '\n'
+							counter += 1
 		
-			if licz == 100:
-				stary_s = re.search(re_stary, page_list.get())
+			if counter == 100:
+				stary_s = re.search(re_stary, outputPage.get())
 				if (stary_s.group(1) != text):
-					final = u'{{język linków|francuski}}\nOto lista około stu najczęściej występujących haseł w języku francuskim, których nie ma jeszcze na Wikisłowniku. Jeśli możesz - dodaj je. Lista ta jest wyborem słów z zestawienia stworzonego przez [http://corpus.leeds.ac.uk/list.html korpus University of Leeds].\nOstatnia aktualizacja: %s\n%s[[Kategoria:Listy frekwencyjne|francuski]]' % (data, text)
-					page_list.put(final, comment=u'Aktualizacja listy')
+					final = u'{{język linków|francuski}}\nOto lista około stu najczęściej występujących haseł w języku francuskim, których nie ma jeszcze na Wikisłowniku. Jeśli możesz - dodaj je. Lista ta jest wyborem słów z zestawienia stworzonego przez [http://corpus.leeds.ac.uk/list.html korpus University of Leeds].\nOstatnia aktualizacja: %s\n%s[[Kategoria:Listy frekwencyjne|francuski]]' % (date, text)
+					outputPage.put(final, comment=u'Aktualizacja listy')
 				return 0
 	
 	
