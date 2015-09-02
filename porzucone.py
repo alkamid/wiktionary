dd56a4fe3893b2ff87a1ed678b309c338e7f1ddb
alkamid <adamkli@gmail.com>
2015-09-02 11:07:58 +0100

    cometics, comments and fix #12

--- porzucone.py
+++ porzucone.py
@@ -1,4 +1,3 @@
-#!/usr/bin/python
 # -*- coding: utf-8 -*-
 
 # find Polish orphans - pages that are not linked by other pages from
@@ -9,18 +8,20 @@ import pywikibot
 from pywikibot import pagegenerators
 import datetime
 import config
+from shutil import move
 
 def main():
 
     site = pywikibot.Site()
-
+    
+    # fetch Polish pages only
     cat_allpages = Category(site,'Kategoria:polski (indeks)')
+    # dialects are excluded, it would be too difficult to link all of them
     cat_dialects = Category(site, 'Kategoria:Polski_(dialekty_i_gwary)')
 
     list_allpages = pagegenerators.CategorizedPageGenerator(cat_allpages)
     list_dialects = set(pagegenerators.CategorizedPageGenerator(cat_dialects, recurse=True))
 
-    lista = []
     count_all = 0
     
     intro = ('<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN"\n'
@@ -36,19 +37,19 @@ def main():
               'słowa, np. w przykładach lub pokrewnych.')
     
  
-    with open('{0}public_html/porzucone.html'.format(config.path['home']), 'w', encoding='utf-8') as f:
+    with open('{0}public_html/porzucone.html.1'.format(config.path['home']), 'w', encoding='utf-8') as f:
         f.write(intro)
         for page in list_allpages:
             if page not in list_dialects:
-                refs = list(page.getReferences(namespaces=0, total=2))
-                try: refs.remove(page)
+                refs = list(page.getReferences(namespaces=0, total=2)) # only look in the main namespace (because virtually all pages are references somewhere, e.g. in missing pronunciation lists)
+                try: refs.remove(page) # reference search is limited to 2. Then the page itself is removed (because on pl.wikt we often self-link pages in examples of usage)
                 except ValueError:
                     pass
             
             if len(refs) == 0:
                 try: f.write('\n<br /><a href="http://pl.wiktionary.org/wiki/{0}">{0}</a>'.format(page.title()))
                 except UnicodeEncodeError:
-                    print(page.title())
+                    print('Unicode Error: ', page.title())
                     pass
                 count_all += 1
 
@@ -56,6 +57,8 @@ def main():
         f.write(date_now.strftime("\nOstatnia aktualizacja listy: %Y-%m-%d, %H:%M:%S"))
         f.write('<br />: Licznik porzuconych: {0}'.format(count_all))
         f.write('</body></html')
+
+    move('{0}public_html/porzucone.html.1'.format(config.path['home']), '{0}public_html/porzucone.html'.format(config.path['home']))
 
 if __name__ == '__main__':
     try:
