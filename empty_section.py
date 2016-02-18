c044e5181d52fc777d167b144c2e5b9276197ca4
alkamid <adamkli@gmail.com>
2016-02-18 07:55:55 +0000

    exclude verbs with {{(nie)dokonany od}} and {{zob}
    they're fine to add examples to, but maybe slightly less interesting
    for the first batch

--- empty_section.py
+++ empty_section.py
@@ -1,8 +1,11 @@
 # -*- coding: utf-8 -*-
 
 from klasa import *
+import re
 
 def empty_section(section):
+
+    re_numbers = re.compile(r'\: \(([0-9]\.[0-9])\)\s*(.*)')
 
     empty_content = ['']
     if section == 'przykłady':
@@ -13,7 +16,7 @@ def empty_section(section):
 
     wordlist = getListFromXML('xx', findLatest=True)
 
-    with open('output/empty_sections.txt', 'w') as f:
+    with open('output/empty_sectionsxx.txt', 'w') as f:
         for word in wordlist:
             try: h = Haslo(word)
             except sectionsNotFound:
@@ -25,5 +28,11 @@ def empty_section(section):
                     for lang_section in h.listLangs:
                         if lang_section.lang == 'polski':
                             lang_section.pola()
-                            if any(lang_section.subSections['przykłady'].text empty for empty in empty_content) any(pos in lang_section.subSections['znaczenia'].text for pos in excluded_pos):
-                                f.write(h.title + '\n')
+                            if any(lang_section.subSections['przykłady'].text == empty for empty in empty_content) \
+                               and not any(pos in lang_section.subSections['znaczenia'].text for pos in excluded_pos):
+                               
+                                defs_found = re.findall(re_numbers, lang_section.subSections['znaczenia'].text)
+                                for defn in defs_found:
+                                    if 'dokonany od' not in defn[1] or '{{zob' not in defn[1]:
+                                        f.write(h.title + '\n')
+                                        break
